26aaedc7777cfec0f6f41b6c092021a80c9dd23c
tudorpopovici01 <tudorpopovici212@gmail.com>
2022-11-26 19:49:05 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.showJsonFormat("Popovici", "IS41R", 8.5);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentProject
+{
+    public class Student
+    {
+        private string nume;
+        protected string grup;
+        public double averageMark;
+
+        
+        public string Nume 
+        {
+            get 
+            {
+                return this.nume;
+            }
+
+            set 
+            {
+                this.nume = value;
+            }
+        }
+
+        public string Grup
+        {
+            get
+            {
+                return this.grup;
+            }
+
+            set
+            {
+                this.nume = grup;
+            }
+        }
+
+        public double AverageMark
+        {
+            get { return this.averageMark; }
+            set { this.averageMark = value; }
+        }
+        public Student() 
+        {
+            this.nume = "Popovici Tudor";
+            this.grup = "IS41R";
+            this.averageMark = 9;
+            
+        }
+
+        public void showJsonFormat(String nume, String grup, double averageMark)
+        {
+            Console.WriteLine(
+                "{" +
+                "nume: " + nume + ", " +
+                "grup: " + grup + ", " +
+                "averageMark: " + averageMark +
+               "}");
+        }
+
+        public Student(String nume)
+        {
+            this.nume = nume;
+        }
+
+        public Student(Double averageMark)
+        {
+            this.averageMark = averageMark;
+        }
+
+        public Student(String nume, String grup, Double averageMark)
+        {
+            this.nume = nume;
+            this.grup = grup;
+            this.averageMark = averageMark;
+
+        }
+
+
+    }
+}
